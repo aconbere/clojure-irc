41c1e6e6f8ddcff54b3e021d3cac3f29ccf2e1ab
Anders Conbere <aconbere@gmail.com>
2013-12-06 16:58:33 -0800

    intr

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject irc "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT" }
   :dependencies [
     [org.clojure/clojure "1.5.1"]
     [instaparse "1.2.8"]
     [aleph "0.3.0"]
     [gloss "0.2.2"]
+    [ac "0.1.0-SNAPSHOT"]
   ]
   :main irc.server)
